1cf2bc476e28a2627464f60cffcbd0cc4ad6ca67
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-06-27 13:17:40 +0200

    update for making angula coefficient parsing

--- CMGTools/WMass/data/theory/angularcoeffs/parsetable.py
+++ CMGTools/WMass/data/theory/angularcoeffs/parsetable.py
@@ -1,28 +1,49 @@
 #!/usr/bin/env python
+# usage: python parsetable.py A*_8TeV_atlas.txt
 import os,re,sys
+from array import array
+from math import *
 
 import ROOT
 ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-args = sys.argv[:]
+def parseOneTable(filename,tfile):
+    inputfile = open(filename)
+    lines = inputfile.readlines()
+    for i,line in enumerate(lines):
+        l=line.rstrip()
+        p = re.compile('(-?0\.\d+\spm\s0\.\d+\spm\s0\.\d+)')
+        vals = p.findall(l)
+        etastr = "eta%s_%s" % (etaBins[i],etaBins[i+1])
+        histoname = basename+"_"+etastr
+        histo = ROOT.TH1F(histoname,"",len(ptBins)-1,array('f',ptBins))
+        for b,ptb in enumerate(vals):
+            ai = [float(f) for f in ptb.split(" pm ")]
+            #print "pt bin: [",ptBins[b],",",ptBins[b+1],"] = ",ai
+            histo.SetBinContent(b+1,ai[0])
+            histo.SetBinError(b+1,hypot(ai[1],ai[2]))
+        tfile.WriteTObject(histo)
 
-filename = args[1]
-basename = os.path.basename(filename).split('.')[0]
-print "Creating histograms for ", basename
 
-ptBins = [0.0 , 2.5 , 5.0 , 8.0 , 11.4 , 14.9 , 18.5 , 22.0 , 25.5 , 29.0 , 32.6 , 36.4 , 40.4 , 44.9 , 50.2 , 56.4 , 63.9 , 73.4 , 85.4 , 105 , 132 , 173 , 253 , 600 ]
-etaBins = [0.0, 1.0, 2.0, 3.5]
+if __name__ == '__main__':
 
-tfile = ROOT.TFile.Open(basename+".root","recreate")
+    args = sys.argv[:]
+    if len(args) == 0:
+        print "You must provide at least one file to parse"
+        exit(0)
 
-inputfile = open(filename)
-lines = inputfile.readlines()
-for i,line in enumerate(lines):
-    l=line.rstrip()
-    p = re.compile('(-?0\.\d+\spm\s0\.\d+\spm\s0\.\d+)')
-    vals = p.findall(l)
-    print "Ai for eta bin ",i," = ", vals
-    for b,ptb in enumerate(vals):
-        ai = [float(f) for f in ptb.split(" pm ")]
-        print "pt bin: [",ptBins[b],",",ptBins[b+1],"] = ",ai
+    ptBins = [0.0 , 2.5 , 5.0 , 8.0 , 11.4 , 14.9 , 18.5 , 22.0 , 25.5 , 29.0 , 32.6 , 36.4 , 40.4 , 44.9 , 50.2 , 56.4 , 63.9 , 73.4 , 85.4 , 105 , 132 , 173 , 253 , 600 ]
+    etaBins = ["00", "10", "20", "35"]
+
+    tfile = ROOT.TFile.Open("Ai_8TeV_atlas.root","recreate")
+
+    for fname in args[1:]:
+        print "fname = ", fname
+        basename = os.path.basename(fname).split('.')[0]
+        print "Creating histograms for ", basename
+        parseOneTable(fname,tfile)
+
+    tfile.Close()
+
+    print "DONE."
