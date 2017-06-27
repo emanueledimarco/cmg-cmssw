ebc7e7fe2a21ff488b7b45c190058e86b575e163
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-06-27 19:18:59 +0200

    update for kinematic variables

--- CMGTools/WMass/python/tools/kinematicVars.py
+++ CMGTools/WMass/python/tools/kinematicVars.py
@@ -1,6 +1,7 @@
 from CMGTools.TTHAnalysis.treeReAnalyzer import *
 from CMGTools.WMass.tools.standaloneElectronCalibrator import ElectronCalibrator
 from CMGTools.WMass.tools.PileUpReWeighter import PileUpReWeighter
+from CMGTools.WMass.tools.eventVars_wmass import SimpleVBoson
 import types, os, math
 import numpy as np
 
@@ -11,8 +12,17 @@ class KinematicVars:
         ROOT.gSystem.Load("libCMGToolsWMass.so")
     def initSample(self,nevents,dataset):
         self.isMC = not any(x in dataset for x in "DoubleMu DoubleEl DoubleEG MuEG MuonEG SingleMu SingleEl".split())
+        aifilename = "%s/src/CMGTools/WMass/data/theory/angularcoeffs/Ai_8TeV_atlas.root" % os.environ['CMSSW_BASE']
+        tfile = ROOT.TFile.Open(aifilename)
+        etabins = ["00","10","20","35"]
+        self.Ai = {}
+        for i in range(5):
+            for e in range(len(etabins)-1):
+                aistr = "A%d_8TeV_atlas_eta%s_%s" % (i,etabins[e],etabins[e+1])
+                self.Ai[(i,e)] = tfile.Get(aistr)
     def listBranches(self):
-        mylist = [ ("nA","I"), ("A","F",8,"nA") ]
+        mylist = [ ("nAP","I"), ("AP","F",8,"nAP"),
+                   ("z_y","F"), ("costheta_cs","F"), ("phi_cs","F")]
         self.branches = mylist
         print "self.branches = ",self.branches[:]
         return self.branches[:]
@@ -39,7 +49,9 @@ class KinematicVars:
         boostedLep = lminus
         boostedLep.Boost(-dilep.BoostVector())
         csframe = self.CSFrame(dilep)
-        return atan2((boostedLep.Vect()*csframe[1]),(boostedLep.Vect()*csframe[0]))
+        phi = atan2((boostedLep.Vect()*csframe[1]),(boostedLep.Vect()*csframe[0]))
+        if(phi<0): return phi + 2*ROOT.TMath.Pi()
+        else: return phi
     def P_i(self,theta,phi,i):
         ct = cos(theta); st = sqrt(1-pow(ct,2))
         cp = cos(phi); sp = sqrt(1-pow(cp,2))
@@ -52,6 +64,15 @@ class KinematicVars:
         elif i==6: return 2*st*ct*sp
         elif i==7: return st*sp
         else: return -999.
+    def A_i(self,i,ptz,yz):
+        if abs(yz)<1: histo = self.Ai[(i,0)]
+        elif abs(yz)<2: histo = self.Ai[(i,1)]
+        else: histo = self.Ai[(i,2)]
+        if histo.GetEntries() == 0: return 0
+        xmin = histo.GetXaxis().GetXmin()+1e-3
+        xmax = histo.GetXaxis().GetXmax()-1e-3
+        xbin = histo.GetXaxis().FindBin(min(max(ptz,xmin),xmax))
+        return histo.GetBinContent(xbin)
     def PtEtaPhiM4V(self,pt,eta,phi,m):
         tlv = ROOT.TLorentzVector()
         tlv.SetPtEtaPhiM(pt,eta,phi,m)
@@ -63,14 +84,18 @@ class KinematicVars:
         leps = [l for l in Collection(event,"LepGood","nLepGood")]
         leps_4v=[ self.PtEtaPhiM4V(l.pt,l.eta,l.phi,0.511*e-3) for l in leps ]
         
-        nPol=8
+        ret["nAP"] = 5 if len(leps)>=2 else 0
+        ret["AP"]=[]
         if len(leps)>=2:
-            ret["nA"]=nPol
             (lplus,lminus) = (leps_4v[0],leps_4v[1]) if leps[0].charge>0 else (leps_4v[1],leps_4v[0])
-            theta_cs = self.cosThetaCS(lplus,lminus); phi_cs = self.phiCS(lplus,lminus);
-            for i in range(nPol): 
-                ret["A"].append(self.P_i(i))
-        else: ret["nA"]=0
+            Z = SimpleVBoson(leps_4v[:2])
+            costheta_cs = self.cosThetaCS(lplus,lminus); phi_cs = self.phiCS(lplus,lminus);
+            ret["costheta_cs"] = costheta_cs; ret["phi_cs"] = phi_cs
+            ret["z_y"] = Z.y()
+            for i in range(ret["nAP"]):
+                ret["AP"].append(self.A_i(i,Z.pt(),Z.y())*self.P_i(acos(costheta_cs),phi_cs,i))
+        else:
+            ret["costheta_cs"] = ret["phi_cs"] = ret["z_y"] = -999
 
         ### return
         return ret
