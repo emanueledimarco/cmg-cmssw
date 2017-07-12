c769863a49ea506f728ad4dfe52dbafd9e286558
Marco Cipriani <cippy91@gmail.com>
2017-07-12 16:47:42 +0200

    add option to choose queue for job resubmission; pass low and high mass index instead of nmasses (nmasse=high-low, but low id can be > 0)

--- CMGTools/WMass/python/plotter/wmass_e/DatacardsTools.py
+++ CMGTools/WMass/python/plotter/wmass_e/DatacardsTools.py
@@ -4,13 +4,14 @@ import sys,ROOT,os
 ROOT.gROOT.SetBatch(True)
 
 class CardsChecker:
-    def __init__(self, card_dir, n_masses):
+    def __init__(self, card_dir, mass_id_down, mass_id_up, queue):
         self.card_dir = card_dir
+        self.queue = queue
         self.subdirs = [os.path.basename(x[0]) for x in os.walk(card_dir) if "eta" in x[0]]
         self.datacards = {}
         self.cardinputs = {}
         for subdir in self.subdirs:
-            for m in range(int(n_masses)):
+            for m in range(int(mass_id_down),int(mass_id_up)+1):
                 for charge in ["neg","pos"]:
                     key = "wenu_mass%d_%s_%s" % (m,charge,subdir)
                     f_txt = subdir+"/"+key+".card.txt" 
@@ -24,7 +25,7 @@ class CardsChecker:
             if not os.path.exists(self.card_dir+"/"+dc): 
                 print "datacard ",dc," is not present in ",self.card_dir
                 resubcmds[key] = "bsub -q {queue} -o {dir}/{logfile} {dir}/{srcfile}".format(
-                    queue="8nh", dir="/".join([os.getcwd(),self.card_dir,"jobs"]), logfile=key+"_resub.log", srcfile=key+".sh")
+                    queue=self.queue, dir="/".join([os.getcwd(),self.card_dir,"jobs"]), logfile=key+"_resub.log", srcfile=key+".sh")
         for key,f in self.cardinputs.iteritems():
             f_ok = True
             if not os.path.exists(self.card_dir+"/"+f): 
@@ -37,18 +38,19 @@ class CardsChecker:
                     f_ok = False
             if not f_ok: 
                 resubcmds[key] = "bsub -q {queue} -o {dir}/{logfile} {dir}/{srcfile}".format(
-                    queue="8nh", dir="/".join([os.getcwd(),self.card_dir,"jobs"]), logfile=key+"_resub.log", srcfile=key+".sh")
+                    queue=self.queue, dir="/".join([os.getcwd(),self.card_dir,"jobs"]), logfile=key+"_resub.log", srcfile=key+".sh")
         return resubcmds
 
 if __name__ == "__main__":
     from optparse import OptionParser
     parser = OptionParser(usage="%prog [options] [inputs]")
     parser.add_option("-c", "--check-cards", dest="check", default=False, action="store_true", help="Check if there are all the datacards and ROOT files are fine");
+    parser.add_option("-q", "--queue", dest="queue", type="string", default="8nh", help="choose the queue to submit batch jobs (default is 8nh)");
     (options, args) = parser.parse_args()
 
     if options.check:
-        if len(args)<2: print "needed inputs: datacards_dir nmasses"; exit(1)
-        cc = CardsChecker(args[0],args[1])
+        if len(args)<3: print "needed inputs: datacards_dir mass_id_down mass_id_up"; quit()
+        cc = CardsChecker(args[0],args[1], args[2], options.queue)
         result = cc.checkCards()
         if len(result)==0: print "All cards are GOOD."
         else: 
