18041d9d3beca448fb400c33c5d60364a77c2890
mdunser <marc.dunser@cern.ch>
2019-12-04 14:39:22 +0100

    fix submission error from userproxy thing

--- PhysicsTools/HeppyCore/scripts/run_condor_simple.sh
+++ PhysicsTools/HeppyCore/scripts/run_condor_simple.sh
@@ -40,7 +40,8 @@ scriptName=${1:-./batchScript.sh}
 cat > $jobdesc <<EOF
 Universe = vanilla
 Executable = ${prefix}${scriptName}
-use_x509userproxy = \$ENV(X509_USER_PROXY)
+use_x509userproxy = true
+x509userproxy = \$ENV(X509_USER_PROXY)
 Log        = ${prefix}condor_job_\$(ProcId).log
 Output     = ${prefix}condor_job_\$(ProcId).out
 Error      = ${prefix}condor_job_\$(ProcId).error
